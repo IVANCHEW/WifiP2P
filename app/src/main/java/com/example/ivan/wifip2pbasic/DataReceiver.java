499f606ad00627373d605e1ddfcb4eda2cb9c410
Ivan <ivanchew92@gmail.com>
2017-04-04 19:31:13 +0800

    Code Cleanup

--- app/src/main/java/com/example/ivan/wifip2pbasic/DataReceiver.java
+++ app/src/main/java/com/example/ivan/wifip2pbasic/DataReceiver.java
@@ -22,7 +22,6 @@ public class DataReceiver implements Runnable {
     //Server Component
     private int port;
     WifiP2pInfo wifiP2pInfo;
-    int previewFormat = 17;
     ServerSocket serverSocket = null;
     Socket socket = null;
     InputStream is;
@@ -45,6 +44,8 @@ public class DataReceiver implements Runnable {
     public void run(){
         Log.d(TAG,"Initialising Data Receiver Class");
         serviceEnabled = true;
+
+        //  SERVER SOCKET CONFIGURATION
         try{
             serverSocket = new ServerSocket(port);
             serverSocket.setPerformancePreferences(0, 1, 1);
@@ -54,7 +55,7 @@ public class DataReceiver implements Runnable {
             Log.d("NEUTRAL", "Sever Service IO Exception Error: " + e.getMessage());
         }
 
-        //STANDARD OPENING CODES
+        // SERVER SOCKET INITIALISATION
         try {
             socket = serverSocket.accept();
             is = socket.getInputStream();
@@ -62,149 +63,33 @@ public class DataReceiver implements Runnable {
             Log.d("NEUTRAL", "Sever Service IO Exception Error: " + e.getMessage());
         }
 
-        //Receiver Loop
+        // BEGIN RECEIVER LOOP
+        int audioBufSize = dm.getAudioBufSize();
+
         while(serviceEnabled){
 
             //Log.d("NEUTRAL", "Server Service: Package Received");
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            byte[] buffer = new byte[1024];
-            byte[] picture_length_buffer = new byte[4];
-            int read = 0;
             int picture_length;
-            int marker = 0;
-            boolean loaded_bytes = false;
-            int available_bytes=0;
-            int audioBufSize = dm.getAudioBufSize();
 
             //Log.d(TAG,"Waiting for first packet");
-
-            try{
-                while (is.available() < 4){
-                }
-            }catch(IOException e) {
-                Log.d("NEUTRAL", "Sever Service IO Exception Error: " + e.getMessage());
-            }
+            ensureAvailable(4);
 
             //Log.d(TAG,"Reading First Packet");
-            try{
-                is.read(picture_length_buffer, 0, 4);
-            }catch(IOException e) {
-                Log.d("NEUTRAL", "Sever Service IO Exception Error: " + e.getMessage());
-            }
-
-            picture_length = byteArrayToInt(picture_length_buffer);
-            //Log.d("NEUTRAL","Length Received: " + picture_length);
+            picture_length = readPictureLength();
+            //Log.d(TAG,"Target Length: " + picture_length);
 
             //Log.d(TAG,"Waiting for second packet");
-            /*
-            try{
-                while (is.available() <= picture_length){
-                    //Log.d(TAG,"Checking length: " + is.available());
-                }
-            }catch(IOException e) {
-                Log.d("NEUTRAL", "Sever Service IO Exception Error: " + e.getMessage());
-            }
-            */
+            ensureAvailable(picture_length);
 
-            Log.d(TAG,"Target Length: " + picture_length);
-            while(!loaded_bytes){
-                try{
-                    available_bytes = is.available();
-                }catch(IOException e) {
-                    Log.d("NEUTRAL", "Sever Service IO Exception Error: " + e.getMessage());
-                }
-                Log.d(TAG,"Available Bytes: " + available_bytes);
-                if (available_bytes>=picture_length){
-                    loaded_bytes = true;
-                    Log.d(TAG,"Inputstream bytes ready");
-                }else{
-                    try{
-                        Thread.sleep(100);
-                        Log.d(TAG,"Sleeping Thread");
-                    }catch (InterruptedException e) {
-                        Log.d(TAG,"Error sleeping thread: " + e.toString());
-                    }
-                }
-            }
+            //waitForImageMemory();
 
-            /*
-            while(dm.getImageLoadStatus()){
-                try{
-                    Thread.sleep(100);
-                    Log.d(TAG,"Sleeping Thread: Image Loading");
-                }catch (InterruptedException e) {
-                    Log.d(TAG,"Error sleeping thread: " + e.toString());
-                }
-            }
-            */
-
-
-            if (!dm.getImageLoadStatus()){
-                //Log.d(TAG,"Reading second packet");
-                while (marker < picture_length){
-
-                    if (picture_length - marker >= 1024){
-                        read = 1024;
-                    }else{
-                        read = picture_length - marker;
-                    }
-                    marker = marker + read;
-                    try{
-                        is.read(buffer,0,read);
-                        //is.skip(1);
-                    }catch(IOException e) {
-                        Log.d("NEUTRAL", "Sever Service IO Exception Error: " + e.getMessage());
-                    }
-                    baos.write(buffer,0,read);
-                    //Log.d("NEUTRAL", "Marker position: " + marker);
-                }
-
-                byte[] buffer2 = baos.toByteArray();
-                Log.d("NEUTRAL","Length of data received: " + buffer2.length);
-
-                Log.d(TAG,"Loading Image");
-                dm.loadImage(buffer2);
-            }
-
-            else{
-                try{
-                    is.skip(picture_length);
-                    Log.d(TAG,"Skipped Frame");
-                }catch(IOException e) {
-                    Log.d("NEUTRAL", "Sever Service IO Exception Error: " + e.getMessage());
-                }
-            }
-
+            readImageData(picture_length);
             ensureAvailable(audioBufSize);
+            readAudioData(audioBufSize);
 
-            if (!dm.getAudioLoadStatus()){
-                byte[] buffer3 = new byte[audioBufSize];
-                boolean audio_read = false;
-                try {
-                    if (is.available() >= audioBufSize) {
-                        Log.d(TAG,"Reading Audio");
-                        is.read(buffer3, 0, audioBufSize);
-                        audio_read = true;
-                    }
-                }catch(IOException e) {
-                    Log.d("NEUTRAL", "Sever Service IO Exception Error: " + e.getMessage());
-                }
-
-                if(audio_read){
-                    Log.d(TAG,"Loading Audio");
-                    dm.loadAudio(buffer3);
-                }
-            }
-            else{
-                try{
-                    is.skip(audioBufSize);
-                    Log.d(TAG,"Skipped Audio Frame");
-                }catch(IOException e) {
-                    Log.d("NEUTRAL", "Sever Service IO Exception Error: " + e.getMessage());
-                }
-            }
         }
 
+        // EXIT LOOP - CLOSE SOCKET
         Log.d(TAG,"Closing Socket");
         try{
             socket.close();
@@ -254,4 +139,100 @@ public class DataReceiver implements Runnable {
         }
     }
 
+    private int readPictureLength(){
+        byte[] picture_length_buffer = new byte[4];
+        int picture_length;
+
+        try{
+            is.read(picture_length_buffer, 0, 4);
+        }catch(IOException e) {
+            Log.d("NEUTRAL", "Sever Service IO Exception Error: " + e.getMessage());
+        }
+
+        picture_length = byteArrayToInt(picture_length_buffer);
+
+        return picture_length;
+    }
+
+    private void readImageData(int picture_length){
+        int marker = 0;
+        int read;
+        byte[] buffer = new byte[1024];
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        if (!dm.getImageLoadStatus()){
+            //Log.d(TAG,"Reading second packet");
+            while (marker < picture_length){
+
+                if (picture_length - marker >= 1024){
+                    read = 1024;
+                }else{
+                    read = picture_length - marker;
+                }
+                marker = marker + read;
+                try{
+                    is.read(buffer,0,read);
+                    //is.skip(1);
+                }catch(IOException e) {
+                    Log.d("NEUTRAL", "Sever Service IO Exception Error: " + e.getMessage());
+                }
+                baos.write(buffer,0,read);
+                //Log.d("NEUTRAL", "Marker position: " + marker);
+            }
+
+            byte[] buffer2 = baos.toByteArray();
+            Log.d("NEUTRAL","Length of data received: " + buffer2.length);
+
+            Log.d(TAG,"Loading Image");
+            dm.loadImage(buffer2);
+        }
+
+        else{
+            try{
+                is.skip(picture_length);
+                Log.d(TAG,"Skipped Frame");
+            }catch(IOException e) {
+                Log.d("NEUTRAL", "Sever Service IO Exception Error: " + e.getMessage());
+            }
+        }
+    }
+
+    private void readAudioData(int audioBufSize){
+        if (!dm.getAudioLoadStatus()){
+            byte[] buffer3 = new byte[audioBufSize];
+            boolean audio_read = false;
+            try {
+                if (is.available() >= audioBufSize) {
+                    Log.d(TAG,"Reading Audio");
+                    is.read(buffer3, 0, audioBufSize);
+                    audio_read = true;
+                }
+            }catch(IOException e) {
+                Log.d("NEUTRAL", "Sever Service IO Exception Error: " + e.getMessage());
+            }
+
+            if(audio_read){
+                Log.d(TAG,"Loading Audio");
+                dm.loadAudio(buffer3);
+            }
+        }
+        else{
+            try{
+                is.skip(audioBufSize);
+                Log.d(TAG,"Skipped Audio Frame");
+            }catch(IOException e) {
+                Log.d("NEUTRAL", "Sever Service IO Exception Error: " + e.getMessage());
+            }
+        }
+    }
+
+    private void waitForImageMemory(){
+        while(dm.getImageLoadStatus()){
+            try{
+                Thread.sleep(100);
+                Log.d(TAG,"Sleeping Thread: Image Loading");
+            }catch (InterruptedException e) {
+                Log.d(TAG,"Error sleeping thread: " + e.toString());
+            }
+        }
+    }
 }
